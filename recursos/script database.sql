73e2561f6e8d45c1df136d513f1760f35da44961
Jaime Cepeda <jr.cepeda.cc@gmail.com>
2023-02-21 16:13:32 -0500

    Creacion de tabla productos

--- recursos/script database.sql
+++ recursos/script database.sql
@@ -2,3 +2,9 @@ create database bd_api_productos;
 
 use bd_api_productos;
 
+create table producto(
+   id INT NOT NULL AUTO_INCREMENT,
+   nombre VARCHAR(100) NOT NULL,
+   precio float NOT NULL,
+   PRIMARY KEY ( tutorial_id )
+);
